3c83362a2c8392d9c35912bdf9728224baca8e40
Vladislav Veluga <vladislav805@yandex.com>
2025-05-15 10:48:27 +0300

    fix: Fix dev-server endpoint

--- src/client/lib/apiRequest.ts
+++ src/client/lib/apiRequest.ts
@@ -1,8 +1,6 @@
-const endpoint = process.env.NODE_ENV === 'production' ? './' : `https://radio.veluga.dev/`;
-
 export async function apiRequest<T>(method: string, params: Record<string, string>): Promise<T> {
     const strParams = new URLSearchParams(params).toString();
-    const response = await fetch(`${endpoint}api/${method}?${strParams}`);
+    const response = await fetch(`./api/${method}?${strParams}`);
     const json = await response.json();
 
     return json.result as T;
